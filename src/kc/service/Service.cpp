21614260222a82c584339f350f224d1a38ea77ab
nekoffski <kacperczyz@protonmail.com>
2021-12-18 14:14:24 +0100

    Move impl to cpp file

---
+++ src/kc/service/Service.cpp
@@ -0,0 +1,29 @@
+#include "Service.h"
+
+#include "kc/core/Log.h"
+
+namespace kc::service {
+
+Service::Service(const std::string& name)
+    : m_name(name)
+    , m_isRunning(true) {
+}
+
+void Service::run() {
+    using namespace std::chrono_literals;
+
+    while (m_isRunning) {
+        update();
+        std::this_thread::sleep_for(50ms);
+    }
+}
+
+void Service::stop() {
+    LOG_INFO("Forcing service {} to stop.", m_name);
+    m_isRunning = false;
+}
+
+std::string Service::getName() const {
+    return m_name;
+}
+}
--- src/kc/service/Service.h
+++ src/kc/service/Service.h
@@ -4,16 +4,11 @@
 #include <string>
 #include <thread>
 
-#include "kc/core/Log.h"
-
 namespace kc::service {
 
 class Service {
 public:
-    Service(const std::string& name)
-        : m_name(name)
-        , m_isRunning(true) {
-    }
+    Service(const std::string& name);
 
     virtual ~Service() = default;
 
@@ -21,23 +16,10 @@ public:
     virtual void onInit() = 0;
     virtual void onShutdown() = 0;
 
-    void run() {
-        using namespace std::chrono_literals;
+    void run();
+    void stop();
 
-        while (m_isRunning) {
-            update();
-            std::this_thread::sleep_for(50ms);
-        }
-    }
-
-    void stop() {
-        LOG_INFO("Forcing service {} to stop.", m_name);
-        m_isRunning = false;
-    }
-
-    std::string getName() const {
-        return m_name;
-    }
+    std::string getName() const;
 
 protected:
     std::string m_name;
