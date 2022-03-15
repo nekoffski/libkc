006e6a6f396b30442b8fdb5b19393dda41409d2c
nekoffski <kacperczyz@protonmail.com>
2022-03-15 18:02:27 +0100

    Fix mock definition

--- test/mocks/ServiceMock.h
+++ test/mocks/ServiceMock.h
@@ -7,7 +7,7 @@
 struct ServiceMock : public kc::service::Service {
     using kc::service::Service::Service;
 
-    MOCK_METHOD(void, update, (), (override));
+    MOCK_METHOD(void, call, (), (override));
     MOCK_METHOD(void, onInit, (), (override));
     MOCK_METHOD(void, onShutdown, (), (override));
 };
