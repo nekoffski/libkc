166608d250ae86865807c184fa6042ca50a9609f
nekoffski <kacperczyz@protonmail.com>
2022-05-26 16:06:15 +0200

    Implement join; Add template specialization for const char* for contains

--- src/kc/core/Utils.hpp
+++ src/kc/core/Utils.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <algorithm>
+#include <cstring>
+#include <sstream>
 
 namespace kc::core {
 
@@ -19,4 +21,21 @@ template <typename T, typename Allocator, template <typename, typename> typename
 bool contains(const Container<T, Allocator>& container, const T& value) {
     return std::find(container.begin(), container.end(), value) != container.end();
 }
+
+template <typename Allocator, template <typename, typename> typename Container>
+bool contains(const Container<const char*, Allocator>& container, const char* const& value) {
+    auto comparator = [value](const char* oth) { return std::strcmp(oth, value) == 0; };
+    return std::find_if(container.begin(), container.end(), comparator) != container.end();
+}
+
+template <typename T, typename Allocator, template <typename, typename> typename Container>
+std::string join(const Container<T, Allocator>& container, const std::string& separator) {
+    std::stringstream stream;
+
+    for (const auto& value : container) stream << value << separator;
+
+    auto output = stream.str();
+    return output.substr(0, output.size() - separator.size());
+}
+
 }  // namespace kc::core
--- test/core/UtilsTests.cpp
+++ test/core/UtilsTests.cpp
@@ -4,6 +4,7 @@
 
 struct UtilsTests : ::testing::Test {
     std::vector<int> elements = {1, 2, 3, 4};
+    std::vector<const char*> elements2 = {"aaa", "bbb", "ccc"};
 };
 
 TEST_F(UtilsTests,
@@ -30,4 +31,30 @@ TEST_F(UtilsTests, givenVectorWithValue_whenCheckingIfContainsValue_shouldReturn
 
 TEST_F(UtilsTests, givenVectorWithoutValue_whenCheckingIfContainsValue_shouldReturnFalse) {
     EXPECT_FALSE(kc::core::contains(elements, 1337));
+}
+
+TEST_F(UtilsTests,
+       givenConstCharPointerVectorWithValue_whenCheckingIfContainsValue_shouldReturnTrue) {
+    std::string text = "bbb";
+    EXPECT_TRUE(kc::core::contains(elements2, text.c_str()));
+}
+
+TEST_F(UtilsTests,
+       givenConstCharPointerVectorWithoutValue_whenCheckingIfContainsValue_shouldReturnFalse) {
+    std::string text = "ddd";
+    EXPECT_FALSE(kc::core::contains(elements2, text.c_str()));
+}
+
+TEST_F(UtilsTests, givenVectorOfInt_whenJoining_shouldReturnCorrectString) {
+    std::vector<int> values = {1, 2, 3};
+    std::string expectedValue = "1, 2, 3";
+
+    EXPECT_EQ(kc::core::join(values, ", "), expectedValue);
+}
+
+TEST_F(UtilsTests, givenVectorOfConstCharPtr_whenJoining_shouldReturnCorrectString) {
+    std::vector<const char*> values = {"aa", "bb", "cc"};
+    std::string expectedValue = "aa,bb,cc";
+
+    EXPECT_EQ(kc::core::join(values, ","), expectedValue);
 }
