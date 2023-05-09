65f18f1881e311ca91ad47dc3ec04031b1b20e8a
nekoffski <kacperczyz@protonmail.com>
2023-05-09 23:32:10 +0200

    add libfmt to deps

--- bin/deps.sh
+++ bin/deps.sh
@@ -7,3 +7,4 @@ sudo apt-get install -y libspdlog-dev
 sudo apt-get install -y libboost-system1.71-dev libboost-coroutine1.71-dev
 sudo apt-get install -y uuid-dev
 sudo apt-get install -y libglm-dev
+sudo apt-get install -y libfmt-dev
