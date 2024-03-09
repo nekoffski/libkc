8b2805bd2571f2058004bfc8b51192e1de5c92f8
nekoffski <kacperczyz@protonmail.com>
2024-03-09 22:38:39 +0100

    Deserialize glm::mat4 from json

--- src/kc/core/Meta.hpp
+++ src/kc/core/Meta.hpp
@@ -8,23 +8,35 @@ namespace kc::core {
 
 template <typename T, typename U, typename V>
 struct is_one_of2
-    : std::integral_constant<bool, std::is_same<T, U>::value || std::is_same<T, V>::value> {};
+    : std::integral_constant<
+        bool, std::is_same<T, U>::value || std::is_same<T, V>::value> {};
 
 template <typename T, typename U, typename V>
 constexpr bool is_one_of2_v = is_one_of2<T, U, V>::value;
 
 template <typename T, typename U, typename V, typename W>
-struct is_one_of3 : std::integral_constant<
-                        bool, std::is_same<T, U>::value || std::is_same<T, V>::value ||
-                                  std::is_same<T, W>::value> {};
+struct is_one_of3
+    : std::integral_constant<
+        bool, std::is_same<T, U>::value || std::is_same<T, V>::value
+                || std::is_same<T, W>::value> {};
 
 template <typename T, typename U, typename V, typename W>
 constexpr bool is_one_of3_v = is_one_of3<T, U, V, W>::value;
 
+template <typename T, typename U, typename V, typename W, typename X>
+struct is_one_of4
+    : std::integral_constant<
+        bool, std::is_same<T, U>::value || std::is_same<T, V>::value
+                || std::is_same<T, W>::value || std::is_same<T, X>::value> {};
+
+template <typename T, typename U, typename V, typename W, typename X>
+constexpr bool is_one_of4_v = is_one_of4<T, U, V, W, X>::value;
+
 template <typename T, typename U>
 using enable_for_impl = std::enable_if<std::is_same<T, U>::value, U>;
 
-template <bool Condition, typename U> using enable_if = typename std::enable_if<Condition, U>::type;
+template <bool Condition, typename U>
+using enable_if = typename std::enable_if<Condition, U>::type;
 
 // clang-format off
 #define enable_for(_type) typename enable_for_impl<T, _type>::type
@@ -32,12 +44,13 @@ template <bool Condition, typename U> using enable_if = typename std::enable_if<
 
 template <typename... Args, std::size_t... Index>
 auto any_match_impl(std::tuple<Args...> const& lhs, std::tuple<Args...> const& rhs, std::index_sequence<Index...>)
-    -> bool {
+  -> bool {
     return (... | (std::get<Index>(lhs) == std::get<Index>(rhs)));
 }
 
 template <typename... Args>
-auto any_match(std::tuple<Args...> const& lhs, std::tuple<Args...> const& rhs) -> bool {
+auto any_match(std::tuple<Args...> const& lhs, std::tuple<Args...> const& rhs)
+  -> bool {
     return any_match_impl(lhs, rhs, std::index_sequence_for<Args...>{});
 }
 
--- src/kc/json/Utils.hpp
+++ src/kc/json/Utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "Json.h"
 #include "kc/core/ErrorBase.hpp"
@@ -14,6 +15,8 @@ DEFINE_SUB_ERROR(JsonLogicError, JsonError);
 namespace detail {
 using namespace core;
 
+// TODO: rewrite to concepts and refactor in general
+
 template <typename Error>
 requires std::derived_from<Error, core::ErrorBase>
 void kc_assert(bool condition, const std::string& errorMessage) {
@@ -21,42 +24,50 @@ void kc_assert(bool condition, const std::string& errorMessage) {
 }
 
 template <typename Error, typename T>
-enable_for(float) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(float)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isDouble(), messagePrefix + " has wrong type");
     return node.asFloat();
 }
 
 template <typename Error, typename T>
-enable_for(int) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(int)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isInt(), messagePrefix + " has wrong type");
     return node.asInt();
 }
 
 template <typename Error, typename T>
-enable_for(std::string) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(std::string)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isString(), messagePrefix + " has wrong type");
     return node.asString();
 }
 
 template <typename Error, typename T>
-enable_for(unsigned int) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(unsigned int)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isUInt(), messagePrefix + " has wrong type");
     return node.asUInt();
 }
 
 template <typename Error, typename T>
-enable_for(bool) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(bool)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isBool(), messagePrefix + " has wrong type");
     return node.asBool();
 }
 
 template <typename Error, typename T>
-enable_for(glm::vec2) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(glm::vec2)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isArray(), messagePrefix + " has wrong type");
     kc_assert<Error>(node.size() == 2, messagePrefix + " has not enough members");
 
     for (auto& child : node)
-        kc_assert<Error>(child.isNumeric(), messagePrefix + " member has invalid type");
+        kc_assert<Error>(
+          child.isNumeric(), messagePrefix + " member has invalid type"
+        );
 
     return glm::vec2{
         node[0].asFloat(),
@@ -65,37 +76,63 @@ enable_for(glm::vec2) tryToExtractValue(const Node& node, const std::string& mes
 }
 
 template <typename Error, typename T>
-enable_for(glm::vec3) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(glm::vec3)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isArray(), messagePrefix + " has wrong type");
     kc_assert<Error>(node.size() == 3, messagePrefix + " has not enough members");
 
     for (auto& child : node)
-        kc_assert<Error>(child.isNumeric(), messagePrefix + " member has invalid type");
+        kc_assert<Error>(
+          child.isNumeric(), messagePrefix + " member has invalid type"
+        );
 
-    return glm::vec3{node[0].asFloat(), node[1].asFloat(), node[2].asFloat()};
+    return glm::vec3{ node[0].asFloat(), node[1].asFloat(), node[2].asFloat() };
 }
 
 template <typename Error, typename T>
-enable_for(glm::vec4) tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+enable_for(glm::vec4)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
     kc_assert<Error>(node.isArray(), messagePrefix + " has wrong type");
     kc_assert<Error>(node.size() == 4, messagePrefix + " has not enough members");
 
     for (auto& child : node)
-        kc_assert<Error>(child.isNumeric(), messagePrefix + " member has invalid type");
+        kc_assert<Error>(
+          child.isNumeric(), messagePrefix + " member has invalid type"
+        );
 
-    return glm::vec4{node[0].asFloat(), node[1].asFloat(), node[2].asFloat(), node[3].asFloat()};
+    return glm::vec4{
+        node[0].asFloat(), node[1].asFloat(), node[2].asFloat(), node[3].asFloat()
+    };
+}
+
+template <typename Error, typename T>
+enable_for(glm::mat4)
+  tryToExtractValue(const Node& node, const std::string& messagePrefix) {
+    kc_assert<Error>(node.isArray(), messagePrefix + " has wrong type");
+    kc_assert<Error>(node.size() == 16, messagePrefix + " has not enough members");
+
+    for (auto& child : node) {
+        kc_assert<Error>(
+          child.isNumeric(), messagePrefix + " member has invalid type"
+        );
+    }
+
+    glm::mat4 out;
+    auto iterator = glm::value_ptr(out);
+    for (int i = 0; i < 16; ++i) iterator[i] = node[i].asFloat();
+    return out;
 }
 
 template <typename Error, typename T>
 requires std::is_default_constructible_v<T>
 class ValueWrapperBase {
-   public:
-    explicit ValueWrapperBase(const T& value, const std::string& fieldName)
-        : m_value(value), m_fieldName(fieldName) {}
+public:
+    explicit ValueWrapperBase(const T& value, const std::string& fieldName) :
+        m_value(value), m_fieldName(fieldName) {}
 
     T get() { return m_value; }
 
-   protected:
+protected:
     T m_value;
     const std::string& m_fieldName;
 };
@@ -108,7 +145,9 @@ struct ValueWrapper<Error, T, typename std::enable_if_t<std::is_arithmetic_v<T>>
     using ValueWrapperBase<Error, T>::ValueWrapperBase;
 
     ValueWrapper&& min(T minValue) && {
-        kc_assert<Error>(this->m_value >= minValue, "Value of field " + this->m_fieldName + "is ");
+        kc_assert<Error>(
+          this->m_value >= minValue, "Value of field " + this->m_fieldName + "is "
+        );
         MOVE_THIS;
     }
 
@@ -117,21 +156,26 @@ struct ValueWrapper<Error, T, typename std::enable_if_t<std::is_arithmetic_v<T>>
         MOVE_THIS;
     }
 
-    ValueWrapper&& inRange(T minValue, T maxValue) && { MOVE_THIS.min(minValue).max(maxValue); }
+    ValueWrapper&& inRange(T minValue, T maxValue) && {
+        MOVE_THIS.min(minValue).max(maxValue);
+    }
 };
 
 template <typename Error, typename T>
 struct ValueWrapper<
-    Error, T, typename std::enable_if_t<is_one_of3_v<T, glm::vec2, glm::vec3, glm::vec4>>>
+  Error, T,
+  typename std::enable_if_t<
+    is_one_of4_v<T, glm::vec2, glm::vec3, glm::vec4, glm::mat4>>>
     : public ValueWrapperBase<Error, T> {
-   public:
+public:
     using ValueWrapperBase<Error, T>::ValueWrapperBase;
 };
 
 template <typename Error, typename T>
-class ValueWrapper<Error, T, typename std::enable_if_t<is_one_of2_v<T, std::string, Node>>>
+class ValueWrapper<
+  Error, T, typename std::enable_if_t<is_one_of2_v<T, std::string, Node>>>
     : public ValueWrapperBase<Error, T> {
-   public:
+public:
     using ValueWrapperBase<Error, T>::ValueWrapperBase;
 
     ValueWrapper&& nonEmpty() && {
@@ -159,55 +203,70 @@ template <typename Error> struct ArrayWrapper : public ValueWrapper<Error, Node>
 
     template <typename U> ArrayWrapper&& ofType() && {
         for (auto& value : this->m_value)
-            tryToExtractValue<Error, U>(value, "Field of array " + this->m_fieldName);
+            tryToExtractValue<Error, U>(
+              value, "Field of array " + this->m_fieldName
+            );
         MOVE_THIS;
     }
 };
 
 template <typename Error> class NodeWrapper {
-   public:
+public:
     explicit NodeWrapper(const Node& node) : m_node(node) {}
 
     NodeWrapper&& withName(const std::string& name) && {
-        kc_assert<JsonLogicError>(not m_name.has_value(), "Name of field is already specified");
-        kc_assert<Error>(m_node.isMember(name), "Could not find field with name: " + name);
+        kc_assert<JsonLogicError>(
+          not m_name.has_value(), "Name of field is already specified"
+        );
+        kc_assert<Error>(
+          m_node.isMember(name), "Could not find field with name: " + name
+        );
         m_name = name;
 
         MOVE_THIS;
     }
 
     template <typename T> ValueWrapper<Error, T> ofType() && {
-        kc_assert<JsonLogicError>(m_name.has_value(), "Field has not specified its name");
+        kc_assert<JsonLogicError>(
+          m_name.has_value(), "Field has not specified its name"
+        );
         auto kc_assertMessagePrefix = "Field with name: " + m_name.value();
 
         return ValueWrapper<Error, T>{
-            tryToExtractValue<Error, T>(m_node.get(m_name.value(), {}), kc_assertMessagePrefix),
-            m_name.value()};
+            tryToExtractValue<Error, T>(
+              m_node.get(m_name.value(), {}), kc_assertMessagePrefix
+            ),
+            m_name.value()
+        };
     }
 
     ArrayWrapper<Error> asArray() {
-        kc_assert<JsonLogicError>(m_name.has_value(), "Field has not specified its name");
+        kc_assert<JsonLogicError>(
+          m_name.has_value(), "Field has not specified its name"
+        );
 
         auto child = m_node.get(m_name.value(), {});
         kc_assert<Error>(
-            child.isArray(), "Field with name " + m_name.value() + " has invalid type"
+          child.isArray(), "Field with name " + m_name.value() + " has invalid type"
         );
 
-        return ArrayWrapper<Error>{child, m_name.value()};
+        return ArrayWrapper<Error>{ child, m_name.value() };
     }
 
     ValueWrapper<Error, Node> asObject() {
-        kc_assert<JsonLogicError>(m_name.has_value(), "Field has not specified its name");
+        kc_assert<JsonLogicError>(
+          m_name.has_value(), "Field has not specified its name"
+        );
 
         auto child = m_node.get(m_name.value(), {});
         kc_assert<Error>(
-            child.isObject(), "Field with name " + m_name.value() + " has invalid type"
+          child.isObject(), "Field with name " + m_name.value() + " has invalid type"
         );
 
-        return ValueWrapper<Error, Node>{child, m_name.value()};
+        return ValueWrapper<Error, Node>{ child, m_name.value() };
     }
 
-   private:
+private:
     const Node& m_node;
     std::optional<std::string> m_name;
 };
@@ -217,17 +276,16 @@ template <typename Error> class NodeWrapper {
 template <typename Error>
 requires std::derived_from<Error, core::ErrorBase>
 class NodeHelper {
-   protected:
+protected:
     detail::NodeWrapper<Error> fieldFrom(const Node& node) {
-        return detail::NodeWrapper<Error>{node};
+        return detail::NodeWrapper<Error>{ node };
     }
 };
 
 template <typename Error>
-requires std::derived_from<Error, core::ErrorBase> detail::NodeWrapper<Error> fieldFrom(
-    const Node& node
-) {
-    return detail::NodeWrapper<Error>{node};
+requires std::derived_from<Error, core::ErrorBase> detail::NodeWrapper<Error>
+  fieldFrom(const Node& node) {
+    return detail::NodeWrapper<Error>{ node };
 };
 
 }  // namespace kc::json
